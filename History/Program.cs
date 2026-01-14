21ce3f205a620aaefc74f37a38781c7d00436d42
Гоша <128387899+Gosha3332@users.noreply.github.com>
2026-01-14 18:57:14 +0500

    new commit

--- History/Program.cs
+++ History/Program.cs
@@ -4,3 +4,16 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+public class GitRepository
+{
+    public string Url { get; set; }
+    public string Name { get; set; }
+    public DateOnly LastDateEditRepository { get; set; }
+    public string Language { get; set; }
+    public List<(DateTime Date, int CommitCount)> HistoryCommit { get; set; }
+
+
+
+    GitRepository() { }
+}
