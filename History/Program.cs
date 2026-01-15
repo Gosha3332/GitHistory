6bf465796d3cfd2d9a8f427a2953ffff3fa4ad3d
Гоша <128387899+Gosha3332@users.noreply.github.com>
2026-01-15 07:54:54 +0500

    new commit

--- History/Program.cs
+++ History/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
-//private string tok = "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";
-
 app.MapGet("/", () => "Hello World!");
 
 
