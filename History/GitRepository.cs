51d29406a307f8b9b9ae5b46ef2402edef4e714d
Гоша <128387899+Gosha3332@users.noreply.github.com>
2026-01-20 19:00:17 +0500

    new commit

--- History/GitRepository.cs
+++ History/GitRepository.cs
@@ -1,14 +1,11 @@
 ﻿public class GitRepository
 {
     public string Url { get; set; }
-    public string Name { get; set; }
-    public int StarsCount { get; set; }
-    public DateOnly LastDateEditRepository { get; set; }
-    public string Language { get; set; }
-    public List<(DateTime Date, int CommitCount)> HistoryCommit { get; set; }
+    public string Name { get; private set; }
+    public int StarsCount { get; private set; }
+    public DateOnly LastDateEditRepository { get; private set; }
+    public string Language { get; private set; }
+    public List<(DateTime Date, int CommitCount)> HistoryCommit { get; private set; }
 
-
-
-
-    GitRepository() { }
+    public GitRepository(string Url) { this.Url = Url; }
 }
--- History/Program.cs
+++ History/Program.cs
@@ -1,7 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/getRepositoryGit/{owner}/{repo}", async (string owner, string repo) =>
+{
+    string token = "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";
+    HttpClient client = new HttpClient();
+    try
+    {
+        HttpResponseMessage resultJson = await client.GetAsync($"https://api.github.com/user?access_token={token}/{owner}/{repo}");
+        if (resultJson != null)
+        {
+            GitRepository repository = new GitRepository
+            (
+                $"https://api.github.com/user?access_token={token}/{owner}/{repo}"
+            );
+        }
+        else { return "Такого репозитория нету"; }
+    }
+    catch (Exception e) { Console.WriteLine(e); }
+    return null;
+});
 
 app.Run();
